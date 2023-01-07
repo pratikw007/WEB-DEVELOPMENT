4ca399dac22a5352a7d27f878bc047d2e8603d56
Emma Paris <eparis@atlassian.com>
2023-01-07 23:43:00 +0530

    commit

--- JS File/file16.js
+++ JS File/file16.js
@@ -2,12 +2,12 @@
 
 let fruits =["Apple","Mango","grapes","Banana"];
 
-// for(let i=0; i<9;i++){
-//     console.log(i);
-//     }
+for(let i=0; i<9;i++){
+    console.log(i);
+    }
     
-// console.log(fruits.length)
-// console.log(fruits[fruits.length-4]);
+console.log(fruits.length)
+console.log(fruits[fruits.length-4]);
 for(let i = 0;i <= fruits.length-1;i++){
     console.log(fruits[i].toUpperCase());  
 }
