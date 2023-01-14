72ceac88179a24c8e4e291222a760caf8dbcbff3
Emma Paris <eparis@atlassian.com>
2023-01-14 23:10:10 +0530

    commit

--- JS File/file30.js
+++ JS File/file30.js
@@ -1,48 +1,94 @@
 // function in js
 
-function singHappyBirthday(){
-    console.log("hello");
-    console.log("hello");
-    console.log("hello");
-    console.log("hello");
-    console.log("hello");
-    console.log("hello");
-    console.log("hello");
-    console.log("hello");
-}
-
-singHappyBirthday();
+// function singHappyBirthday(){
+//     console.log("hello");
+//     console.log("hello");
+//     console.log("hello");
+//     console.log("hello");
+//     console.log("hello");
+//     console.log("hello");
+//     console.log("hello");
+//     console.log("hello");
+// }
 
 
-function Sum(){
-    console.log(2+3);
-}
 
-Sum();
+// singHappyBirthday();
 
-function sum1(){
-    return 2+9;
-}
-console.log(sum1());
 
-function Sum3(num1,num2){
-    return num1 + num2;
-}
+// function Sum(){
+//     console.log(2+3);
+// }
 
-const sumhai = Sum3(5,8); //num1 = 2 ,num2 = 6
-console.log(sumhai);
+// Sum();
+
+// function sum1(){
+//     return 2+9;
+// }
+// console.log(sum1());
+
+// function Sum3(num1,num2){
+//     return num1 + num2;
+// }
+
+// const sumhai = Sum3(5,8); //num1 = 2 ,num2 = 6
+// console.log(sumhai);
 
 // isEven  
 // input : 1 number
 // output : true ,false
 
-function isEven(number){
-    if(number%2===0){
-        console.log("true");
-    }
-    else{
-        console.log("false");
-    }
-}
+// function isEven(number){
+//     if(number%2===0){
+//         console.log("true");
+//     }
+//     else{
+//         console.log("false");
+//     }
+// }
+// function isEven(num){
+//  return num % 2 === 0;
+// }
+// console.log(isEven(50));
 
-isEven(2);
+//Que: function 
+// input : String
+// output:firstCharecter
+
+// function firstCharecter(anystring){
+//     return anystring[0]; //0th index print karenga string ka
+// }
+
+// console.log(firstCharecter("Hello"));
+
+// Que: function 
+// input : array,target(number)
+// output : index of target if target present in array
+// [1,5,9,27,6]  find 9 index
+
+
+// function findTarget(array,target){
+//     for(let i = 0;i<array.length;i++){
+//         if(array[i]===target){
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
+
+// const myArray = [1,3,8,90];
+// const ans = findTarget(myArray, 90);//3- lenth of arr
+// console.log(ans);
+
+
+// Arrow function
+
+// function singHappyBirthday = () => {
+//     console.log("hello");
+// }
+
+// singHappyBirthday();
+
+const  sumOfThree(num1,num2,num3){
+
+}
