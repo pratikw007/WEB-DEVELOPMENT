6da11be561764fa911bdc355604aa83f518fba21
Emma Paris <eparis@atlassian.com>
2023-01-17 23:04:03 +0530

    commit

--- JS File/file39.js
+++ JS File/file39.js
@@ -3,8 +3,8 @@
 const numbers = [4,2,5,8];
 
 function muliplyBy2(number,index){
-    // console.log("index is ", index);
-    // console.log(number*2); //8
+    console.log("index is ", index);
+    console.log(number*2); //8
     console.log(`index is ${index} number is ${number}`);
     // console.log(`${number}*2 = ${number*2}`); //4*2 = 8
 }
