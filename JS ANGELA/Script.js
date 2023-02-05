acdf7386dd65b489968daf713e7b589427da9a7a
Emma Paris <eparis@atlassian.com>
2023-02-05 18:30:52 +0530

    day 1

---
+++ JS ANGELA/Script.js
@@ -0,0 +1,36 @@
+// var myName = "Angela";
+// var yourName = prompt("What is your name?");
+//     alert("My Name is" + myName + ", Welcome to my course"+ yourName+";");
+
+// var ganeLevel = 1;
+// ganeLevel = 2;
+// ganeLevel = 3;
+// alert("Your Leevel is Currently : " + ganeLevel);
+
+// function test() {
+//     var a = "3";
+//     var b = "8";
+// // **************************
+// // Do Not change the code 
+//     var temp = a;
+//     a = b;
+//     b = temp;
+//     // *************************
+//     console.log("a is "+ a);
+//     console.log("b is "+ b);
+// }
+
+// test();
+//***************name convention************
+// var Myname = "Angela";
+// var yourName = "Jack bauer";
+// var myvar = "abc";
+// var my123 = 123;
+// var myName = "Angela";
+// var abc$_ = true;
+
+// alert("Welcome!"+"Pratik") 
+
+
+var name ="Pratik";
+name.slice(0,1);
