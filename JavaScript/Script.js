9712d04984706a562bd911ed8ad52eff64631cca
Emma Paris <eparis@atlassian.com>
2023-04-09 11:31:55 +0530

    Dom-II

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -1,48 +1,98 @@
-console.log("Hello");    
-let a = 5;
-console.log(a);
-var b = "Hey";
-console.log(b)
+// console.log("Hello");    
+// let a = 5;
+// console.log(a);
+// var b = "Hey";
+// console.log(b)
 
-// var is globle and let isn't globle
-
-
-if (a==5) {
-    var x = 5;
-    let y = 54;
-    console.log(x);
-    console.log(y);
-}
-
-    console.log(x);
-    // console.log(y); // not run properly
-
-    // const name = "Pratik";
-    // name = "ram"; we can't change
+// // var is globle and let isn't globle
 
 
-    console.log(5>3);
-    console.log(5<3);
+// if (a==5) {
+//     var x = 5;
+//     let y = 54;
+//     console.log(x);
+//     console.log(y);
+// }
 
-    let age = 18;
-    let status = (age>18)?"I Can Vote": "I Can't vote";
-    console.log(status);
+//     console.log(x);
+//     // console.log(y); // not run properly
 
-
-
-    let marks = 89;
-    if (marks>=90) {
-        console.log('A');
-    }
-    else if(marks >= 80){
-        console.log('B')
-    }
-    else if (marks>= 60) {
-        console.log('C')
-    }
-    else{
-        console.log('Failed!!')
-    }
+//     // const name = "Pratik";
+//     // name = "ram"; we can't change
 
 
-      
+//     console.log(5>3);
+//     console.log(5<3);
+
+//     let age = 18;
+//     let status = (age>18)?"I Can Vote": "I Can't vote";
+//     console.log(status);
+
+
+
+//     let marks = 89;
+//     if (marks>=90) {
+//         console.log('A');
+//     }
+//     else if(marks >= 80){
+//         console.log('B')
+//     }
+//     else if (marks>= 60) {
+//         console.log('C')
+//     }
+//     else{
+//         console.log('Failed!!')
+//     }
+
+
+      // function eventFunction(){
+      //   console.log ('I Have clicked on the document');
+      // }
+
+
+      // document.addEventListener('click',eventFunction);
+
+      // const content = document.querySelector('click',function(event){
+      //     console.log(event);
+      // }); 
+
+      // -------------------------------------
+ 
+let myDiv = document.createElement('div');
+
+
+// function ParaStatus(event){
+  
+//   console.log('para'+event.target.textContent)
+// }
+
+// myDiv.addEventListener('click',ParaStatus);
+
+// for(let i=1;i<=100;i++){
+//   let newElement =document.createElement('p');//create listner
+//   newElement.textContent = 'This is para' + i;
+//   // add event lisner
+//   myDiv.appendChild(newElement);
+// }
+
+// document.body.appendChild(myDiv)
+
+// -------------------------------------------------
+
+
+// let element = document.querySelectorAll('#wrapper');
+
+// element.addEventListener('click', function(event){
+//   if (event.target.nodeName === 'SPAN'){
+//     console.log('Span pr click kia hai' + event.target.textContent)
+//   }
+// })
+
+
+
+
+
+
+
+
+
